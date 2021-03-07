6d2473dfda36db502eaab4ec217d8c8ac0ead7e4
reallycoolfrog <62034606+reallycoolfrog@users.noreply.github.com>
2021-03-07 15:50:52 -0500

    Add files via upload

---
+++ Scripts/EndBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBehavior : MonoBehaviour
+{
+    SpriteRenderer myRenderer;
+    public Sprite endSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Player"){ //Collide w/ player
+            myRenderer.sprite = endSprite;
+        } 
+    }
+}
